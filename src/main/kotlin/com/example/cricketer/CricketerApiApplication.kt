8121126e3aa7e0be3c4253f8420b02f3fd6baffb
Nirmal <kagosekar@gmail.com>
2022-10-25 12:13:34 +0530

    rename incorrect class name

--- src/main/kotlin/com/example/cricketer/CricketerApiApplication.kt
+++ src/main/kotlin/com/example/cricketer/CricketerApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BookmarkerApiApplication
+class CricketerApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<BookmarkerApiApplication>(*args)
+	runApplication<CricketerApiApplication>(*args)
 }
